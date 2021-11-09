dbf8ff7200865dff6d64456d5e1800b7fed31ca7
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-11-09 22:17:26 +0330

    not work

--- infix_to_postfix.cpp
+++ infix_to_postfix.cpp
@@ -1,4 +1,4 @@
-#include <cstring>
+#include <string>
 #include <iostream>
 using namespace std;
 
@@ -26,7 +26,47 @@ class Stack {
 
     stack_ptr = new T[capacity];
   }
+  // Stack(const Stack& obj) {
+  //   top = obj.top;
+  //   capacity = obj.capacity;
+
+  //   stack_ptr = new T[capacity];
+  //   while (!is_empty()) {
+  //     obj.push(obj.pop());
+  //   }
+  //   }
   ~Stack() { delete[] stack_ptr; }
+
+  const Stack& operator=(const Stack& stk) {
+    if (this != &stk) {
+      delete[] stack_ptr;
+      top = stk.top;
+      capacity = stk.capacity;
+      stack_ptr = new T[capacity];
+      for (int i = 0; i <= top; i++) {
+        stack_ptr[i] = stk.stack_ptr[i];
+      }
+    }
+    return *this;
+  }
+
+  // Stack operator+(const Stack& stk) {
+  //   Stack temp(capacity + stk.capacity);
+  //   for (int i = 0; i <= top; i++) {
+  //     temp.stack_ptr[i] = stack_ptr[i];
+  //   }
+
+  //   for (int i = 0; i <= stk.top; i++) {
+  //     temp.stack_ptr[i + top + 1] = stk.stack_ptr[i];
+  //   }
+  //   // while (!is_empty()) {
+  //   //   temp.push(pop());
+  //   // }
+  //   // while (!stk.is_empty()) {
+  //   //   temp.push(stk.pop());
+  //   // }
+  //   return temp;
+  // }
 
   bool push(const T& item) {
     if (is_full()) {
@@ -52,6 +92,20 @@ class Stack {
     return stack_ptr[top];
   }
 
+  string get_str() const {
+    string str = "";
+    for (int i = 0; i <= top; i++) {
+      char item = stack_ptr[i];
+      if (item == NULL) {
+        break;
+      }
+      str += item;
+    }
+    return str;
+  }
+
+  int get_size() const { return top; }
+
   bool is_empty() const { return (top == -1); }
   bool is_full() const { return top == capacity - 1; }
 
@@ -62,19 +116,55 @@ class Stack {
   T* stack_ptr;
 };
 
-Stack<char> infix_to_postfix(const char* expression) {
-  int length = strlen(expression);
+Stack<char> infix_to_postfix(const string expression) {
+  int length = expression.length();
   Stack<char> operators(length);
   Stack<char> result(length + 10);
 
   for (int i = 0; i < length; i++) {
-    // cout << expression[i];
     switch (expression[i]) {
       case '(':
-        operators.push(expression[i]);
-        break;
+
+        {
+          // Stack<char> temp(length - i - 1);
+          Stack<char> temp(10);
+          // cout << "SUBSTR: " << expression.substr(i + 1, length) << endl;
+          temp = infix_to_postfix(expression.substr(i + 1, length));
+          // cout << "TEMP STR: " << temp.get_str() << ";;;" << endl;
+          // cout << "TEMP STR: " << result.get_str() << ";;;" << endl;
+          string str = temp.get_str();
+          for (int j = 0; j < str.length(); j++) {
+            result.push(str[j]);
+          }
+          i = result.get_size() + i + 2;
+          // cout << "TEMP SIZE: " << i << endl;
+          // cout << "RESULT STR: " << result.get_str() << ";;;" << endl;
+          // cout << "RESULT LENGTH: " << result.get_size() << ";;;" << endl;
+          // cout << "str LENGTH: " << str.length() << ";;;" << endl;
+          // cout << "str : " << str << ";;;" << endl;
+          // cout << "EXPRESSION: " << expression << endl;
+          // cout << "i: " << i << endl;
+          // cout << "EXPRESSION[i]: " << expression[i] << endl;
+          // cout << "EXPRESSION: " << expression << endl;
+
+        // operators.push(expression[i]);
+        // cout << "Test--------" << endl;
+        // cout << expression.substr(i + 1, length) << endl;
+        // cout << endl << "Test--------" << endl;
+        break;}
       case ')':
-        /* code */
+      // cout << "------------------------------------" << endl;
+      // cout << "EXPRESSION: " << expression;
+  
+
+      while(!operators.is_empty()) {
+        char a = operators.pop();
+        result.push(a);
+      }
+      //   cout << endl << "RESULT: " << result ;
+      // cout << endl << "------------------------------------" << endl;
+        
+        return result;
         break;
       case '^':
         while (operators.peak() == '^') {
@@ -132,7 +222,5 @@ Stack<char> infix_to_postfix(const char* expression) {
 }
 
 int main() {
-  // char* b = "hello world";
-  // cout << strlen(b);
-  cout << infix_to_postfix("a^b*d+f/g");
+  cout << infix_to_postfix("a+(b*c^d)^(e-f)/g");
 }
