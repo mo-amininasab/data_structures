6085f6447a8f31e341910417ebdde11cd42f1c4e
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-11-10 12:01:30 +0330

    done

--- infix_to_postfix2.cpp
+++ infix_to_postfix2.cpp
@@ -17,7 +17,7 @@ class Stack {
     }
 
     for (int i = 0; i <= obj.top; i++) {
-      out << obj.stack_ptr[i] << "  ";
+      out << obj.stack_ptr[i] << "";
     }
     out << endl;
 
@@ -70,19 +70,19 @@ class Stack {
     return stack_ptr[top];
   }
 
-  string get_str() const {
-    string str = "";
-    for (int i = 0; i <= top; i++) {
-      char item = stack_ptr[i];
-      if (item == NULL) {
-        break;
-      }
-      str += item;
-    }
-    return str;
-  }
+  // string get_str() const {
+  //   string str = "";
+  //   for (int i = 0; i <= top; i++) {
+  //     char item = stack_ptr[i];
+  //     if (item == NULL) {
+  //       break;
+  //     }
+  //     str += item;
+  //   }
+  //   return str;
+  // }
 
-  int get_size() const { return top; }
+  // int get_size() const { return top; }
 
   bool is_empty() const { return (top == -1); }
   bool is_full() const { return top == capacity - 1; }
@@ -94,9 +94,81 @@ class Stack {
   T* stack_ptr;
 };
 
+int prec(char ch) {
+  switch (ch) {
+    case '+':
+    case '-':
+      return 1;
+    case '*':
+    case '/':
+      return 2;
+    case '^':
+      return 3;
+    default:
+      return -1;
+  }
+}
 
+bool is_operand(char ch) {
+  return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+}
+
+Stack<char> infix_to_postfix(const string expression) {
+  int length = expression.length();
+  Stack<char> operators(length);
+  Stack<char> result(length + 5); // +5 just to be safe.
+
+  for (int i = 0; i < length; i++) {
+    if (is_operand(expression[i])) {  // if operand
+      result.push(expression[i]);
+
+    } else if (expression[i] == '(') {
+      operators.push(expression[i]);
+
+    } else if (expression[i] == ')') {
+      while (!operators.is_empty() && operators.peak() != '(') {
+        result.push(operators.pop());
+      }
+
+      operators.pop();
+
+    } else {  // if operator
+      while (!operators.is_empty() &&
+             prec(expression[i]) <= prec(operators.peak())) {
+        result.push(operators.pop());
+      }
+
+      operators.push(expression[i]);
+    }
+  }
+  // pop all the operators.
+  while (!operators.is_empty()) {
+    result.push(operators.pop());
+  }
+
+  return result;
+}
 
 int main() {
+  string expression1 = "(a+b)*(c+d)";
+  cout << expression1 << "  --->  ";
+  cout << infix_to_postfix("(a+b)*(c+d)") << endl;
+
+  string expression2 = "a+(b*c^d)^(e-f)/g";
+  cout << expression2 << "  --->  ";
+  cout << infix_to_postfix("a+(b*c^d)^(e-f)/g") << endl;
+
+  string expression3 = "a^(b+c)-a/d*e";
+  cout << expression3 << "  --->  ";
+  cout << infix_to_postfix("a^(b+c)-a/d*e") << endl;
+
+  string expression4 = "a+b^c*d";
+  cout << expression4 << "  --->  ";
+  cout << infix_to_postfix("a+b^c*d") << endl;
+
+  string expression5 = "a+b/(c*d+e)-f^g";
+  cout << expression5 << "  --->  ";
+  cout << infix_to_postfix("a+b/(c*d+e)-f^g") << endl;
 
   return 0;
 }
