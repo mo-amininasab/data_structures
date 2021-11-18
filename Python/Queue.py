a8036a779a7502e1e8355be5f2352b7800345cf3
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-11-18 14:43:23 +0330

    Queue in Python added

--- Python/Queue.py
+++ Python/Queue.py
@@ -1,24 +1,21 @@
 # %%
-class Stack:
+class Queue:
   def __init__(self):
     self.items = []
 
   def isEmpty(self):
     return self.items == []
 
-  def push(self, item):
-    self.items.append(item)
+  def enqueue(self, item):
+    self.items.insert(0, item)
 
-  def pop(self):
+  def dequeue(self):
     if self.isEmpty():
       return None
     return self.items.pop()
 
-  def peek(self):
-    return self.items[len(self.items) - 1]
-
   def size(self):
     return len(self.items)
 
-  def __str__(self) -> str:
+  def __str__(self):
     return str(self.items)
